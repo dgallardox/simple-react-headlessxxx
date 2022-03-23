6226a023f6996754ec01e5c96a2c2993743db368
dgallardox <diego.gallardo@wpengine.com>
2022-03-23 14:06:07 -0500

    env var

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from './components/Post';
 
 function App() {
+
+  const posts = [
+    {
+      id: 1,
+      title: "test",
+    },
+    {
+      id: 2,
+      title: "test2",
+    },
+    {
+      id: 3,
+      title: "test3"
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My Posts</h2>
+
+      {posts.map((post) => (
+        < Post title={post.title} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,8 @@
+
+export default function Post(props) {
+  return (
+    <article>
+      <h3>{props.title}</h3>
+    </article>
+  )
+}
